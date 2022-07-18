aa6be222b98255a8e5462fdfca90e265eb84be5e
CharlieGaribay <calos.garibayc@gmail.com>
2022-07-17 19:20:33 -0500

    SignUp Page added social network section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import logo100Ladrillos from './logo_100_ladrillos.svg'
 import Signup from './components/Signup';
+import google from './google.png';
+import facebook from './facebook.png';
+import microsoftWindowsLogo from './microsoft-windows-logo.png';
 
 export default class App extends Component {
 
@@ -11,6 +14,39 @@ export default class App extends Component {
       width: '100%',
     }
   }
+
+  stylesHr() {
+    return {
+      border: "1px solid #eaeaea",
+      height: "1px",
+      marginTop: "11px",
+      marginLeft: "13px",
+      width: "84.4px",
+    }
+  }
+  stylesSocialNetworkContainer() {
+    return {
+      border: "1px solid #eaeaea",
+      height: "48x",
+      width: "48px",
+      textAlign: "center",
+      padding: "8px",
+    }
+  }
+  stylesSocialNetworkImages() {
+    return {
+      width: "32px",
+      height: "32px",
+    }
+  }
+
+  stylesSigin() {
+    return {
+      color: "#ff5a60",
+      fontWeight: "bold",
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,12 +58,51 @@ export default class App extends Component {
           <div className="col-12 col-lg-6">
             <img alt="imagen" />
           </div>
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-lg-5">
             <h3>Crear tu cuenta</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quas, dolores sit minus quod tempore reprehenderit facilis, provident earum eaque eum delectus temporibus, eveniet autem eos itaque debitis atque voluptate?
-            </p>
+            <div>
+              <p>
+                Al aceptar crear una cuenta en 100 Ladrillos <br />
+                aceptas nuestro <a href="https://100ladrillos.zendesk.com/hc/es/articles/360001074632-Aviso-de-Privacidad">Aviso de Privacidad</a>, <a href="https://100ladrillos.zendesk.com/hc/es/articles/360001106571-Derechos-ARCO"> Derechos Arco </a>
+                y nuestros <a href="https://100ladrillos.zendesk.com/hc/es/articles/360001099131-T%C3%A9rminos-y-Condiciones">Términos y Condiciones</a>.
+              </p>
+            </div> <br />
+
             <Signup />
+
+            <div className="row">
+              <div className="col" style={this.stylesHr()}></div>
+              <div className="col-8 text-center">o regístrate con:</div>
+              <div className="col" style={this.stylesHr()}></div>
+            </div>
+
+            <br />
+
+            <div className="row">
+              <div className="col-4">
+                <div style={this.stylesSocialNetworkContainer()}>
+                  <img src={google} alt="Google" style={this.stylesSocialNetworkImages()} />
+                </div>
+              </div>
+              <div className="col-4">
+                <div style={this.stylesSocialNetworkContainer()}>
+                  <img src={microsoftWindowsLogo} alt="Microsoft Windows Logo" style={this.stylesSocialNetworkImages()} />
+                </div>
+              </div>
+              <div className="col-4">
+                <div style={this.stylesSocialNetworkContainer()}>
+                  <img src={facebook} alt="Facebook" style={this.stylesSocialNetworkImages()} />
+                </div>
+              </div>
+            </div>
+
+            <br />
+
+            <div className="text-center">
+              <span>¿Ya tienes tu cuenta?</span> <br />
+              <a href="/" style={this.stylesSigin()}>Iniciar sesión</a>
+            </div>
+
           </div>
         </div>
       </div>
